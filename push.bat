1e809122246988ca4fa28e8e89aa429e732387f3
huangqiang <huangqiang97@126.com>
2024-08-17 16:23:14 +0800

    20240817-162314

--- push.bat
+++ push.bat
@@ -1,44 +1,14 @@
 @echo off
-setlocal enabledelayedexpansion
+for /f %%i in ('powershell -Command "Get-Date -Format \"yyyyMMdd-HHmmss\""') do set timestamp=%%i
 
-REM 获取当前日期时间，并格式化为 "yyyymmdd-hhmmss"
-for /f "tokens=1-3 delims=/- " %%a in ('echo %date%') do (
-    set year=%%c
-    set month=%%a
-    set day=%%b
-)
-
-REM 如果年份在前面，调整解析顺序
-if !year! LSS 100 (
-    set temp=!year!
-    set year=!month!
-    set month=!day!
-    set day=!temp!
-)
-
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do (
-    set hour=%%a
-    set minute=%%b
-)
-set second=%time:~6,2%
-
-REM 处理个位数的月和日，确保是两位数
-if !month! LSS 10 set month=0!month!
-if !day! LSS 10 set day=0!day!
-
-REM 处理个位数的小时，确保是两位数
-if !hour! LSS 10 set hour=0!hour!
-
-set timestamp=!year!!month!!day!-!hour!!minute!!second!
+REM 打印出生成的时间戳以确认格式正确
+echo %timestamp%
 
 REM 将当前文件夹下所有文件添加到Git暂存区
 git add .
 
 REM 提交并使用当前时间作为提交信息
-git commit -m "!timestamp!"
+git commit -m "%timestamp%"
 
 REM 推送到远程仓库
 git push
-
-endlocal
-@echo on
