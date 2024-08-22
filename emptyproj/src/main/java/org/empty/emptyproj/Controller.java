8b0e7140e4abc53c2408094f48724c315c1bd5fb
huangqiang <huangqiang97@126.com>
2024-08-22 17:59:50 +0800

    20240822-175950

--- emptyproj/src/main/java/org/empty/emptyproj/Controller.java
+++ emptyproj/src/main/java/org/empty/emptyproj/Controller.java
@@ -17,6 +17,8 @@ import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -27,13 +29,23 @@ public class Controller {
 
     @PostConstruct
     public void initFlowRules() {
-        List<FlowRule> rules = new ArrayList<>();
-        FlowRule rule = new FlowRule();
-        rule.setResource("manualResource");
-        rule.setCount(16); // 每秒最多2次访问
-        rule.setControlBehavior(RuleConstant.CONTROL_BEHAVIOR_WARM_UP);
-        rules.add(rule);
-        FlowRuleManager.loadRules(rules);
+        List<FlowRule> flowRules = new ArrayList<>();
+        FlowRule flowRule = new FlowRule();
+        flowRule.setResource("manualResource");
+        flowRule.setCount(16); // 每秒最多2次访问
+        flowRule.setControlBehavior(RuleConstant.CONTROL_BEHAVIOR_WARM_UP);
+        flowRules.add(flowRule);
+        FlowRuleManager.loadRules(flowRules);
+
+        List<DegradeRule> degradeRules = new ArrayList<>();
+        DegradeRule degradeRule = new DegradeRule();
+        degradeRule.setResource("manualResource");  // 指定资源名称
+        degradeRule.setGrade(RuleConstant.DEGRADE_GRADE_RT);  // 设置降级策略，这里使用RT（平均响应时间）
+        degradeRule.setCount(100);  // 设置阈值，例如100ms的平均响应时间
+        degradeRule.setTimeWindow(10);  // 设置降级后的恢复时间窗口，单位为秒
+        degradeRules.add(degradeRule);
+        DegradeRuleManager.loadRules(degradeRules);
+
     }
 
     @GetMapping("/time")
